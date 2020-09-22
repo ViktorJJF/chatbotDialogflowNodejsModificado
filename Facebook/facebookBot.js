abee9bb5ee33c89869c766c3b5959d3b5e7a93e6
ViktorJJF <vj.jimenez96@gmail.com>
2020-09-22 18:38:38 -0500

    agregamos un boton y una imagen

--- Facebook/facebookBot.js
+++ Facebook/facebookBot.js
@@ -153,6 +153,20 @@ function handleDialogFlowAction(
   parameters
 ) {
   switch (action) {
+    case "Code.DemasElementos.action":
+      sendTextMessage(sender, "Estoy mandando una imagen y un boton");
+      sendImageMessage(
+        sender,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQeOnyjNIucV-XNe6DcdOuhKahh9jdNY4RkuQ&usqp=CAU"
+      );
+      sendButtonMessage(sender, "Ejemplo de boton", [
+        {
+          type: "web_url",
+          url: "https://www.messenger.com",
+          title: "Visit Messenger",
+        },
+      ]);
+      break;
     case "Code.menuCarrusel.action":
       let helados = [
         {
